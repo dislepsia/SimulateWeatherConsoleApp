eb6d2b828e18d9295184e78404aaecc739357f9d
dislepsia <adrianlazarte@live.com.ar>
2019-12-23 11:50:04 -0300

    Creación de la aplicación de consola encargada de realizar la simulación del pronóstico del ejercicio.

---
+++ WeatherConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
