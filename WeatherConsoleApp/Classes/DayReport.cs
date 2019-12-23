32c14e4346e7241f27c46a4330dda7aedd37bd94
dislepsia <adrianlazarte@live.com.ar>
2019-12-23 19:07:44 -0300

    Se agregaron las siguientes clases: -DayReport: Manipula el reporte del clima diario -Weather: Simula el clima de cada dia -WeatherReport: Genera el reporte del clima a 10 años

---
+++ WeatherConsoleApp/Classes/DayReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Weather.Classes
+{
+    public class DayReport
+    {
+        public Weather Weather { get; set; }
+        public int Day { get; set; }
+
+        public DayReport(Weather weather, int day)
+        {
+            Weather = weather;
+            Day = day;
+        }
+    }
+}
